30702764b2db5beed5c0b73c210b300187d9a32c
NaurisB <nauris.blumbahs@gmail.com>
2021-09-14 21:21:54 +0300

    Initial

---
+++ 3.uzdevums/3.uzdevums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3.uzdevums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
